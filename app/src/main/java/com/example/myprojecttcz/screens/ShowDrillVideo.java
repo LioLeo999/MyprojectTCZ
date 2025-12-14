0b6817813c4d30a158635236d105e458f63469a6
dlevinas <levinas.daniel@gmail.com>
2025-12-14 21:21:12 +0200

    tikunshowvideo

--- app/src/main/java/com/example/myprojecttcz/screens/ShowDrillVideo.java
+++ app/src/main/java/com/example/myprojecttcz/screens/ShowDrillVideo.java
@@ -3,6 +3,7 @@ package com.example.myprojecttcz.screens;
 import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,37 +15,68 @@ import com.example.myprojecttcz.R;
 
 public class ShowDrillVideo extends AppCompatActivity {
 
-    private Intent get;
     private WebView webView;
-    private String link, embed;
+    private String link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_show_drill_video);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         initViews();
     }
-    public void initViews(){
-        get = getIntent();
-        link = get.getStringExtra("link");
+
+    private void initViews() {
+        Intent intent = getIntent();
+        link = intent.getStringExtra("link");
+
         webView = findViewById(R.id.youtubevideo);
-        embed = convertToEmbedUrl(link);
-        if (embed != null) {
-            webView.loadUrl(embed);
-        }
 
-
+        // הגדרות חובה ל-YouTube
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        webView.setWebViewClient(new WebViewClient());
 
+        String videoId = extractVideoId(link);
+        if (videoId == null) {
+            finish();
+            return;
+        }
+
+        String html =
+                "<!DOCTYPE html>" +
+                        "<html>" +
+                        "<body style='margin:0'>" +
+                        "<iframe " +
+                        "width='100%' " +
+                        "height='100%' " +
+                        "src='https://www.youtube.com/embed/" + videoId + "' " +
+                        "frameborder='0' " +
+                        "allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' " +
+                        "allowfullscreen>" +
+                        "</iframe>" +
+                        "</body>" +
+                        "</html>".formatted(videoId);
+
+        webView.loadDataWithBaseURL(
+                "https://www.youtube.com",
+                html,
+                "text/html",
+                "utf-8",
+                null
+        );
     }
-    private String convertToEmbedUrl(String youtubeUrl) {
+
+    // חילוץ videoId מכל סוג קישור
+    private String extractVideoId(String youtubeUrl) {
         if (youtubeUrl == null) return null;
 
         String videoId = null;
@@ -59,6 +91,11 @@ public class ShowDrillVideo extends AppCompatActivity {
 
         if (videoId == null) return null;
 
-        return "https://www.youtube.com/embed/" + videoId;
+        // ניקוי פרמטרים מיותרים
+        if (videoId.contains("&")) {
+            videoId = videoId.substring(0, videoId.indexOf("&"));
+        }
+
+        return videoId;
     }
 }
